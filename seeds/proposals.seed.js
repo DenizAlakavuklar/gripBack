5b478f8fc6094f3eb211b6f8fca467807e3c9963
Krystina Witt <krystinawitt@gmail.com>
2023-03-08 13:09:58 +0100

    added some proposal seeds

--- seeds/proposals.seed.js
+++ seeds/proposals.seed.js
@@ -87,13 +87,45 @@ const proposalsArr = [{
   "image": "https://a0.muscache.com/im/pictures/ff1ccf4c-806f-44ed-bf81-7172f7874633.jpg?im_w=960",
   "location": "Shinjuku City, Tokyo, Japan",
   "type": "AirBnB", /* AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment */
-  "totalPrice": 0,
-  "nights": 0,
+  "totalPrice": 200,
+  "nights": 21,
   "link": "https://www.airbnb.com/rooms/737336213626967440?adults=2&check_in=2023-03-12&check_out=2023-04-02&federated_search_id=3f0d65e1-eb3e-499b-b0ec-31bacaaeaa3e&source_impression_id=p3_1677758179_kYOK74obsETakr6Q",
   "link2": "",
   "trip": "64007c198fbd72993f22f19d",
   "votes": ["64007cdd6fbd749d9622f1a8", "64007c456fbd729d9f22f1a2"],
   "createdBy": "64007cdd6fbd749d9622f1a8",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  } , 
+  {
+    "_id": "64087956e5d723cba711aaeb",
+  "title": "3Bdr+Central+Spacious+Netflix+Air-con",
+  "image": "https://a0.muscache.com/im/pictures/miso/Hosting-11872394/original/bf2147c2-1899-498e-97b5-66362c482605.jpeg?im_w=960",
+  "location": "Kutchan, Abuta District, Hokkaido, Japan",
+  "type": "AirBnB", /* AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment */
+  "totalPrice": 356,
+  "nights": 21,
+  "link": "https://www.airbnb.com/rooms/11872394?adults=2&category_tag=Tag%3A7765&check_in=2023-03-08&check_out=2023-03-29&federated_search_id=7f21c884-cc17-477a-b41f-45bc7a3bd495&source_impression_id=p3_1678276915_AI%2BR4EIGJ2lIaJfZ",
+  "link2": "",
+  "trip": "64007c198fbd72993f22f19d",
+  "votes": ["64007c456fbd729d9f22f1a2"],
+  "createdBy": "64007c456fbd729d9f22f1a2",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  } , 
+  {
+    "_id": "640879b0642ca08c5741a5f7",
+  "title": "Private old house inn",
+  "image": "https://a0.muscache.com/im/pictures/be223c9b-0416-43b2-93f2-cfc600cf6a8a.jpg?im_w=960",
+  "location": "Minobu, Minamikoma District, Yamanashi, Japan",
+  "type": "AirBnB", /* AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment */
+  "totalPrice": 155,
+  "nights": 21,
+  "link": "https://www.airbnb.com/rooms/593130866047245545?adults=2&category_tag=Tag%3A8524&check_in=2023-03-08&check_out=2023-03-29&federated_search_id=7f21c884-cc17-477a-b41f-45bc7a3bd495&source_impression_id=p3_1678276919_nxtAudk3Dy40ZO6q",
+  "link2": "",
+  "trip": "64007c198fbd72993f22f19d",
+  "votes": ["64007c456fbd729d9f22f1a2"],
+  "createdBy": "64007c456fbd729d9f22f1a2",
   "createdAt": "2023-03-02T10:36:09.606+00:00", 
   "updatedAt": "2023-03-02T10:36:09.606+00:00"
   } 
@@ -115,6 +147,96 @@ const proposalsArr = [{
   "createdAt": "2023-03-02T10:36:09.606+00:00", 
   "updatedAt": "2023-03-02T10:36:09.606+00:00"
   }
+  , 
+  {
+    "_id": "64087784460daac3b47f9512",
+  "title": "Leafy Greens Chiangmai : Snail Dome (SND)",
+  "image": "https://a0.muscache.com/im/pictures/f6c46cce-0c0b-4ebc-ba01-3cce16527978.jpg?im_w=720",
+  "location": "Tambon Chang Phueak, Chang Wat Chiang Mai, Thailand",
+  "type": "AirBnB", 
+  // AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment
+  "totalPrice": 80,
+  "nights": 4,
+  "link": "https://www.airbnb.com/rooms/32751601?adults=3&category_tag=Tag%3A8225&check_in=2023-03-21&check_out=2023-03-25&federated_search_id=d796c9a1-c5c5-40d9-b53a-b2bf421aa624&source_impression_id=p3_1678276471_4Kpeym2LE5xOI5S3",
+  "link2": "",
+  "trip": "28577c198fcd729d9f22f110",
+  "votes": ["61007cee6fcc75989622f1a8", "54327cdd6fbd749d9622f1a8"],
+  "createdBy": "54327cdd6fbd749d9622f1a8",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  }
+
+  , 
+  {
+    "_id": "640877e8299c5c238febd9bd",
+  "title": "[VR] 2BR High Skyline Ocean View @ Pattaya",
+  "image": "https://a0.muscache.com/im/pictures/1f23e84a-f226-4b5d-9405-ffe291206b13.jpg?im_w=1200",
+  "location": "Muang Pattaya, Chang Wat Chon Buri, Thailand",
+  "type": "AirBnB", 
+  // AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment
+  "totalPrice": 112,
+  "nights": 4,
+  "link": "https://www.airbnb.com/rooms/25986044?adults=3&category_tag=Tag%3A7769&check_in=2023-03-21&check_out=2023-03-25&federated_search_id=d796c9a1-c5c5-40d9-b53a-b2bf421aa624&source_impression_id=p3_1678276593_Xa5gn92nlRUtcnZW",
+  "link2": "",
+  "trip": "28577c198fcd729d9f22f110",
+  "votes": ["24607cdd6fbd222d9622e7b1", "54327cdd6fbd749d9622f1a8"],
+  "createdBy": "24607cdd6fbd222d9622e7b1",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  }
+  /*SURFING IN MORCOO*/
+  ,   {
+    "_id": "64087a3631844efd010f5b1b",
+  "title": "❤️Charming apartment in❤️❤️ Quartier Hivernage❤️",
+  "image": "https://a0.muscache.com/im/pictures/miso/Hosting-549767899995543224/original/b1ff4bf0-7c42-47be-a1ed-46cd6b9f0826.jpeg?im_w=960",
+  "location": "Marrakech, Marrakech-Safi, Morocco",
+  "type": "AirBnB", 
+  // AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment
+  "totalPrice": 95,
+  "nights": 4,
+  "link": "https://www.airbnb.com/rooms/549767899995543224?adults=3&check_in=2023-03-09&check_out=2023-03-12&federated_search_id=33db5e31-5948-46f2-a42b-80bccaa627c9&source_impression_id=p3_1678277142_ciB%2BqQPznmzwBO4x",
+  "link2": "",
+  "trip": "84007c198fcd729d9f22f198",
+  "votes": ["64007cdd6fbd729d9f22f1a8", "64007cca6fbd729d9f22f1a4", "64007cd36fbd729d9f22f1a6"],
+  "createdBy": "64007cdd6fbd729d9f22f1a8",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  } ,   {
+    "_id": "64087a79412cddfd30228db7",
+  "title": "OceanFront Beautiful Condo with Pool/Beach",
+  "image": "https://a0.muscache.com/im/pictures/7953468a-fc66-46ed-802c-32b2b42acaa6.jpg?im_w=1200",
+  "location": "Marrakech, Marrakech-Safi, Morocco",
+  "type": "AirBnB", 
+  // AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment
+  "totalPrice": 89,
+  "nights": 4,
+  "link": "https://www.airbnb.com/rooms/549767899995543224?adults=3&check_in=2023-03-09&check_out=2023-03-12&federated_search_id=33db5e31-5948-46f2-a42b-80bccaa627c9&source_impression_id=p3_1678277142_ciB%2BqQPznmzwBO4x",
+  "link2": "",
+  "trip": "84007c198fcd729d9f22f198",
+  "votes": ["64007cca6fbd729d9f22f1a4", "64007cd36fbd729d9f22f1a6"],
+  "createdBy": "64007cca6fbd729d9f22f1a4",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  }
+
+ /*  , 
+  {
+    "_id": "",
+  "title": "",
+  "image": "",
+  "location": "",
+  "type": "", 
+  // AirBnB, Camping, Crash at a friends, Hotel, Serviced Apartment
+  "totalPrice": 0,
+  "nights": 0,
+  "link": "",
+  "link2": "",
+  "trip": "",
+  "votes": ["", "", "", "", ""],
+  "createdBy": "",
+  "createdAt": "2023-03-02T10:36:09.606+00:00", 
+  "updatedAt": "2023-03-02T10:36:09.606+00:00"
+  } */
   ]
 
   
